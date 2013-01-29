912b3d13d99cd2d4cf38382f7be68101755d075a
twidi <s.angel@twidi.com>
2013-01-29 02:57:57 +0100

    Beautiful account home page

--- js/app.js
+++ js/app.js
@@ -267,7 +267,29 @@ var Reposio = (function() {
     }
 
     Display.prototype.get_markup_for_account_home = function(account) {
-        var markup = '<p><img src="' + account.details.avatar_url + '" /></p><p><strong>' + account.username + '</strong> (<strong>' + account.details.name + '</strong>) est sur <strong>' + account.provider.name + '</strong></p>';
+        var markup = '<ul data-role="listview" data-theme="e" class="account-main"><li>';
+        markup += '<img src="' + account.details.avatar_url + '" />';
+        markup += '<h3>' + account.username + '</h3>';
+        if (account.details.name) {
+            markup += '<p>' + account.details.name + '</p>';
+        }
+        markup += '<p class="ui-li-aside ui-btn-up-c ui-btn-corner-all provider">' + account.provider.name + '</p>'
+        markup += '</li></ul>';
+        markup += '<ul data-role="listview" data-theme="d" class="account-details">';
+        if (account.details.company) {
+            markup += '<li class="ui-li-has-icon"><span class="ui-li-icon ui-icon ui-icon-gear ui-icon-shadow"></span>' + account.details.company + '</li>'
+        }
+        if (account.details.location) {
+            markup += '<li class="ui-li-has-icon"><span class="ui-li-icon ui-icon ui-icon-mappin ui-icon-shadow"></span>' + account.details.location + '</li>'
+        }
+        if (account.details.email && account.details.email.indexOf('@') !== -1) {
+            markup += '<li class="ui-li-has-icon"><span class="ui-li-icon ui-icon ui-icon-email ui-icon-shadow"></span>' + account.details.email + '</li>'
+        }
+        if (account.details.blog) {
+            markup += '<li class="ui-li-has-icon"><span class="ui-li-icon ui-icon ui-icon-link ui-icon-shadow"></span>' + account.details.blog + '</li>'
+        }
+        markup += '<li class="ui-li-has-icon"><span class="ui-li-icon ui-icon ui-icon-calendar ui-icon-shadow"></span>' + account.details.created_at + '</li>'
+        markup += '</ul>'
         return markup;
     }
 

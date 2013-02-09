466073b339f2058f87ba600af0b70627a61c8175
twidi <s.angel@twidi.com>
2013-02-09 22:13:20 +0100

    Avoid duplicate the first page when replacing ! by ?

--- js/jquery.mobile-preconfig.js
+++ js/jquery.mobile-preconfig.js
@@ -1,18 +1,29 @@
-$(document).bind("mobileinit", function(){
-    $.mobile.page.prototype.options.addBackBtn = true;
-    $.mobile.defaultPageTransition = 'slide';
-});
+(function() {
+    $(document).bind("mobileinit", function(){
+        $.mobile.page.prototype.options.addBackBtn = true;
+        $.mobile.defaultPageTransition = 'slide';
+    });
 
-function querystringize(hash) {
-    if (hash.indexOf('!') != -1) {
-        return hash.replace('!', '?');
+    function querystringize(hash) {
+        if (hash.indexOf('!') != -1) {
+            return hash.replace('!', '?');
+        }
+        return hash;
     }
-    return hash;
-}
-location.hash = querystringize(location.hash);
 
-$(document).bind("pagebeforechange", function(e, data) {
-    if (typeof data.toPage === "string") {
-        data.toPage = querystringize(data.toPage);
+    var corrected_hash = querystringize(location.hash);
+    if (corrected_hash != location.hash) {
+        try {
+            window.history.replaceState({}, document.title, corrected_hash);
+        } catch(e) {
+            location.hash = corrected_hash;
+        }
     }
-});
+
+    $(document).bind("pagebeforechange", function(e, data) {
+        if (typeof data.toPage === "string") {
+            data.toPage = querystringize(data.toPage);
+        }
+    });
+
+})();

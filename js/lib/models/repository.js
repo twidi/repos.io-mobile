6065b3938090d4a4d7e9d85112bd6f15a9e28574
twidi <s.angel@twidi.com>
2013-03-07 09:13:33 +0100

    Make filters/options work for forks when all loaded

--- js/lib/models/repository.js
+++ js/lib/models/repository.js
@@ -17,6 +17,14 @@
                 forks: {per_page: 50},
                 stars: {per_page: 50},
                 contributors: {per_page: 100} // no pagination !?
+            },
+            fetchable_params: {
+                forks: ['sort']
+            },
+            sort_and_filter_helpers: {
+                forks: {
+                    sort_field: {newest: 'created_at', oldest: 'created_at', watchers: 'watchers_count'}
+                }
             }
         },
 
@@ -24,6 +32,19 @@
             this.$super(id, controller);
             this.path = this.ref;
             this.href_id = this.id.replace('/', ':');
+        },
+
+        sort_and_filter_forks: function(options, force_data) {
+            var global = this.list_page_status.forks.__global__,
+                helpers = this.$class.sort_and_filter_helpers.forks,
+                data = force_data || this.forks[global.str_params];
+
+            data = _.sortBy(data, helpers.sort_field[options.sort]);
+            if (options.sort != 'oldest') {
+                data.reverse();
+            }
+
+            return data;
         }
 
     });

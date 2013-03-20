aa7d9a10fa08cc8a95b9bed8ffbe638cc38618a6
twidi <s.angel@twidi.com>
2013-03-20 23:52:45 +0100

    cf #13 - Minify css

--- tools/compress.sh
+++ tools/compress.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+function jsmin {
+    slimit -m $1 > $2
+}  
+
+function cssmin {
+    csstidy $1 --template=highest --silent=true $2
+}
+
 # compress js/lib/* to js/lib.min.js
 export LANG=C; cat js/lib/*.js js/lib/*/*.js js/lib/*/*/*.js > js/lib.js
-slimit -m js/lib.js > js/lib.min.js
+jsmin js/lib.js js/lib.min.js
 rm js/lib.js
 
 # concat some libs
@@ -15,4 +23,9 @@ grep -v '^[\/\*]' gh3.min.js >> libraries.min.js
 grep -v '^[\/\*]' classy.min.js >> libraries.min.js
 grep -v '^[\/\*]' screenfull.min.js >> libraries.min.js
 grep -v '^[\/\*]' marked.min.js >> libraries.min.js
+cd ../..
+
+# concat css
+cssmin css/resources/jqm-icon-pack-2.0-original.css css/resources/jqm-icon-pack-2.0-original.min.css
+cssmin css/app.css css/app.min.css
 

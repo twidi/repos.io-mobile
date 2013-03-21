0dd1f6d260f56a0f6dba2a5cea7d025106c57459
twidi <s.angel@twidi.com>
2013-03-21 01:50:52 +0100

    cf #13 - Compress html and use yui-compressor for js/css
    A "non-compressed" html version (with uncompressed js/lib/) is
    created at debug.html (index.html is fully compressed)

---
+++ js/_inline.js
@@ -0,0 +1,30 @@
+(function() {
+    $(document).bind("mobileinit", function(){
+        $.mobile.page.prototype.options.addBackBtn = true;
+        $.mobile.defaultPageTransition = 'slide';
+        $.mobile.autoInitializePage = false;
+    });
+
+    function querystringize(hash) {
+        if (hash.indexOf('!') != -1) {
+            return hash.replace('!', '?');
+        }
+        return hash;
+    }
+
+    var corrected_hash = querystringize(location.hash);
+    if (corrected_hash != location.hash) {
+        try {
+            window.history.replaceState({}, document.title, corrected_hash);
+        } catch(e) {
+            location.hash = corrected_hash;
+        }
+    }
+
+    $(document).bind("pagebeforechange", function(e, data) {
+        if (typeof data.toPage === "string") {
+            data.toPage = querystringize(data.toPage);
+        }
+    });
+
+})();
--- tools/compress.sh
+++ tools/compress.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function jsmin {
-    slimit -m $1 > $2
+    yui-compressor --type js $1 > $2
 }  
 
 function cssmin {
-    csstidy $1 --template=highest --silent=true $2
+    yui-compressor --type css $1 > $2
 }
 
 # compress js/lib/* to js/lib.min.js
@@ -13,19 +13,57 @@ export LANG=C; cat js/lib/*.js js/lib/*/*.js js/lib/*/*/*.js > js/lib.js
 jsmin js/lib.js js/lib.min.js
 rm js/lib.js
 
-# concat some libs
-cd js/resources
-rm -f libraries.min.js
-grep -v '^[\/\*]' jQuery.unserializeForm.min.js >> libraries.min.js
-grep -v '^[\/\*]' jqm.page.params.min.js >> libraries.min.js
-grep -v '^[\/\*]' underscore-min.js >> libraries.min.js
-grep -v '^[\/\*]' gh3.min.js >> libraries.min.js
-grep -v '^[\/\*]' classy.min.js >> libraries.min.js
-grep -v '^[\/\*]' screenfull.min.js >> libraries.min.js
-grep -v '^[\/\*]' marked.min.js >> libraries.min.js
-cd ../..
+# compress and concat some libs
+rm -f js/resources/libraries.min.js
+for js_base_file in jQuery.unserializeForm jqm.page.params underscore gh3 classy screenfull marked
+do
+    normal_file=js/resources/$js_base_file.js
+    min_file=js/resources/$js_base_file.min.js
+    if [ -e $normal_file ]
+    then
+        rm $min_file
+        jsmin $normal_file $min_file
+    fi
+    cat $min_file >> js/resources/libraries.min.js
+done
 
 # concat css
 cssmin css/resources/jqm-icon-pack-2.0-original.css css/resources/jqm-icon-pack-2.0-original.min.css
 cssmin css/app.css css/app.min.css
 
+# min html parts
+for i in 02 04 06 07 08
+do
+    normal_file=parts/$i.html
+    min_file=`echo $normal_file | sed -e 's/.html/.min.html/'`
+    tr -s '[:space:]' ' ' < $normal_file > $min_file
+done
+
+# concat inlines css and js
+rm -f parts/03.* parts/05.*
+cp css/_inline.css parts/03.debug.html
+cssmin css/_inline.css parts/03.html
+cp js/_inline.js parts/05.debug.html
+jsmin js/_inline.js parts/05.html
+
+# compute html pages
+rm -f index.html debug.html
+for i in 01 02 03 04 05 06 07 08
+do
+    base_file=parts/$i.html
+    normal_file=parts/$i.min.html
+    if [ ! -e $normal_file ]
+    then
+        normal_file=$base_file
+    fi    
+    debug_file=parts/$i.debug.html
+    if [ ! -e $debug_file ]
+    then
+        debug_file=$base_file
+    fi
+
+    cat $normal_file >> index.html
+    cat $debug_file >> debug.html
+done
+
+rm -f parts/*.min.html parts/03.* parts/05.*

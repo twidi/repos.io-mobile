152d0310a08e45bfeeca03feb5031776119360b9
twidi <s.angel@twidi.com>
2013-05-14 21:44:08 +0200

    cf #9 - Rename token.py into git_token.py

--- js/providers-config.example.js
+++ js/providers-config.example.js
@@ -2,6 +2,6 @@ var providers_config = {
     github: {
         auth: 'oauth',
         client_id: 'foobar', // you must create an app on github
-        token_script: 'http://path.to/token.py' // careful of CORS !
+        token_script: 'http://path.to/git_token.py' // careful of CORS !
     }
 };
